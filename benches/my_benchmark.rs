bb35e5e4b74e87dd850614ea6da395c955c12951
Seth Stadick <seth_stadick@bio-rad.com>
2019-07-23 07:57:26 -0700

    much nicer api

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -5,22 +5,31 @@ use criterion::Criterion;
 use criterion::black_box;
 
 extern crate hammy;
-use hammy::{hamming, encode_dna};
+use hammy::{hamming, base_bits::*};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let alpha = encode_dna::encode("ACTGACTGACTG");
-    let beta = encode_dna::encode("ACTGGGGGACTG");
+    let alpha = BaseBits::new("ACTGACTGACTG");
+    let beta = BaseBits::new("ACTGGGGGACTG");
     c.bench_function("ham xor pre encoded",
                      move |b| b.iter(||
-                      hamming::hamming_code(alpha, beta)));
+                      hamming::hamming_code(alpha.code, beta.code)));
     c.bench_function("ham xor",
                      |b| b.iter(||
-                      hamming::hamming_code(encode_dna::encode("ACTGACTGACTG"),
-                      encode_dna::encode("ACTGGGGGACTG"))));
+                      hamming::hamming_code(BaseBits::new("ACTGACTGACTG").code,
+                      BaseBits::new("ACTGGGGGACTG").code)));
     c.bench_function("ham str",
                      |b| b.iter(||
                       hamming::hamming_str(black_box("ACTGACTGACTG"),
                                            black_box("ACTGGGGGACTG"))));
+    let a = BaseBits::new("ACTGACTGACTG");
+    let be = BaseBits::new("ACTGGGGGACTG");
+    c.bench_function("BaseBits Pre",
+                      move |b| b.iter(||
+                      hamming_dist(&a, &be)));
+    
+    c.bench_function("BaseBits with encoding",
+                     |b| b.iter(||
+                      hamming_dist(&BaseBits::new("ACTGACTGACTG"), &BaseBits::new("ACTGGGGGACTG"))));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use super::hamming::*;
-    use super::encode_dna::*;
+    use super::base_bits::*;
     #[test]
     fn test_hamming_dist() {
         assert_eq!(hamming_str("ACTG", "ACTT"), 1);
@@ -9,34 +9,71 @@ mod tests {
     }
 
     #[test]
-    fn test_hamming_code() {
-        assert_eq!(hamming_code(encode("ACTG"), encode("ACTT")), 1);
+    fn test_base_bits() {
+        let alpha = BaseBits::new("ACTG");
+        let beta = BaseBits::new("ACTT");
+        assert_eq!(hamming_dist(&alpha, &beta), 1);
     }
 
     #[test]
-    fn test_encode() {
-        let code = encode("ACTG");
-        println!("{:b}", code);
-        assert_eq!(code, 0b000110101011);
+    fn test_encoding() {
+        let bb = BaseBits::new("ACTG");
+        println!("{:b}", bb.code);
+        assert_eq!(bb.code, 0b000110101011);
     }
 }
 
-// Encode
+/// Encode a DNA string of up to 21 bases as a u64 for fast hamming distance calculations.
+/// TODO: Add a bump to use u128 or maybe bigint if 21 chars is not enough. 
+pub mod base_bits {
+    pub const ENCODING_DIST: u32 = 2;
+    pub const ENCODING_LENGTH: u32 = 3;
+    pub const UNDETERMINED: u64 = 0b100;
+    pub const ANY: u64 = 0b111;
 
-pub mod encode_dna {
-    pub fn encode(seq: &str) -> u64 {
-        let mut code: u64 = 0;
-        for c in seq.chars() {
-            code = (code << 3) | match c {
-                'A' => 0b000,
-                'C' => 0b110,
-                'T' => 0b101,
-                'G' => 0b011,
-                'N' => 0b100,
-                 _ => 0b111,
+    struct Bases;
+    impl Bases {
+        const A: u64 = 0b000;
+        const C: u64 = 0b110;
+        const T: u64 = 0b101;
+        const G: u64 = 0b011;
+        const N: u64 = UNDETERMINED;
+        const STAR: u64 = ANY;
+    }
+  
+    //#[derive(Copy, Clone)]
+    pub struct BaseBits {
+        pub code: u64
+    }
+
+    impl BaseBits {
+        pub fn new(seq: &str)-> BaseBits {
+            let mut code: u64 = 0;
+            for c in seq.chars() {
+                code = (code << ENCODING_LENGTH) | match c {
+                    'A' => Bases::A,
+                    'C' => Bases::C,
+                    'T' => Bases::T,
+                    'G' => Bases::G,
+                    'N' => Bases::N,
+                     _ => Bases::STAR,
+                }
             }
+            BaseBits{code}
         }
-        code
+    }
+
+    //impl std::ops::BitXor for &BaseBits {
+        //type Output = u64;
+
+        //fn bitxor(self, rhs: Self) -> Self::Output {
+            //self.code ^ rhs.code
+        //}
+    //}
+
+    #[inline]
+    pub fn hamming_dist(alpha: &BaseBits, beta: &BaseBits) -> u32 {
+        (alpha.code ^ beta.code).count_ones() / 2
     }
 }
 
